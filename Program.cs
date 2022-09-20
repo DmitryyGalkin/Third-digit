78edc6d02fba8287acf0a6df643708c16d9e7811
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-20 21:04:02 +0300

    Third Digit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+string numberStr = number.ToString();
+
+if (numberStr.Length > 2)
+    Console.WriteLine($"Третья цифра {numberStr[2]}");
+else
+    Console.WriteLine("Третьей цифры нет");
+
+
